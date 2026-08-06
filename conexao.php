6a901fd353c6621377ffaa67b9d1a835a4a84e63
Rafael Ramos Araujo <159478325+rafaelramosaraujo@users.noreply.github.com>
2026-08-06 11:51:25 -0300

    aula06/08/2026

---
+++ conexao.php
@@ -0,0 +1,24 @@
+<?php
+    // conexao php
+    $host = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "sistema_produto";
+    $conexao = mysqli_connect($host, $usuario, $senha, $banco);
+
+    // var_dump($conexao);
+
+    // if ($conexao) {
+    //     echo "Conexao realizada com sucesso";
+    // } else {
+    //     echo "Erro ao conectar". mysqli_connect_error();
+    // }
+    
+
+
+
+
+
+
+
+?>
---
+++ rodape.php
@@ -0,0 +1,5 @@
+    <footer>
+        <p>Sistema de Produtos - Todos os direitos reservados</p>
+    </footer>
+</body>
+</html>
