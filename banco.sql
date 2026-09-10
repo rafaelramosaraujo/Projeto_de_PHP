3a54f47c062b0307ee74919e2f01d4ea661fdd9b
Rafael Ramos Araujo <159478325+rafaelramosaraujo@users.noreply.github.com>
2026-09-10 13:39:29 -0300

    AULA07

--- banco.sql
+++ banco.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30-Jul-2026 às 16:48
--- Versão do servidor: 10.4.32-MariaDB
--- versão do PHP: 8.2.12
+-- Tempo de geração: 10-Set-2026 às 16:49
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,7 +35,18 @@ CREATE TABLE `produtos` (
   `descricao` varchar(255) NOT NULL,
   `preco` decimal(10,2) NOT NULL,
   `quantidade` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `descricao`, `preco`, `quantidade`) VALUES
+(1, 'Fone', 'de cor preta e dourada', '28.00', 5),
+(2, 'Microfone', 'de cor preta e prata', '40.00', 7),
+(3, 'Monitor Profissional LG 32', '4K, 60Hz, 4ms, VA, FreeSync, HDR10, Som Integrado - 32UR500-B', '1649.99', 10),
+(4, 'Mouse Gamer Sem Fio Razer Viper V3 Pro', 'Sensor Focus Pro, 35000 DPI, Bateria 95h, Polling Rate 8000Hz, Preto - RZ01-05120100', '1078.99', 9),
+(5, 'Teclado Gamer Logitech G PRO X TKL RAPID', ' Switches Magnético-analógicos, Modo Rapid Trigger, Layout US, Branco - 920-013132', '1149.99', 8);
 
 -- --------------------------------------------------------
 
@@ -48,7 +59,7 @@ CREATE TABLE `usuarios` (
   `nome` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `usuarios`
@@ -81,7 +92,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
